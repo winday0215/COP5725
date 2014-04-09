ac292203810e2f52af1a1b2dd51897b807399f6c
ssingh8008 <ssingh8008@gmail.com>
2014-04-08 22:12:22 -0400

    'q' is not recognized as an internal or external command, operable program or batch file.

--- restaurant/php/signin.php
+++ restaurant/php/signin.php
@@ -1,23 +1,65 @@
-
 <?php
+session_start();
+$valid=false;
 $connection = oci_connect($username = 'jing',
                           $password = 'spring123456',
                           $connection_string = '//oracle.cise.ufl.edu/orcl');
-$statement = oci_parse($connection, 'SELECT * FROM useraccount');
-oci_execute($statement);
 
-echo "<table border='1'>\n";
-while ($row = oci_fetch_array($statement, OCI_ASSOC+OCI_RETURN_NULLS)) {
-    echo "<tr>\n";
-    foreach ($row as $item) {
-        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
-    }
-    echo "</tr>\n";
+if (!$connection) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
-echo "</table>\n";
 
-//
-// VERY important to close Oracle Database Connections and free statements!
-//
+if (!isset ($_SESSION['user']))
+{
+	if (isset ($_POST['submit'])){
+	
+		$valid = true;
+		//If email is userid we can do accordingly
+		$id = $_POST['email'];
+		$password = $_POST['password'];
+		$_SESSION['user']=$id;
+		if(empty($id) || empty($pass)){ $valid = false; }
+		
+		if($valid == true)
+		{	
+			$sql = "select EMAIL from useraccount where EMAIL = '{$id}' AND PASSWORD = '{$password}'";
+			$query = oci_parse($connection,$sql);
+			oci_execute($query);
+			
+			//$query = mysql_query($sql); was the SQL syntax
+			
+			if(oci_num_rows($query) > 0)
+			{
+			$login='Welcome back ';
+			}	
+			else
+			{
+				$login= 'Wrong login or password';
+			}
+
+			if(!$query)
+			{
+				echo $login;
+				// setcookie("user",$id, time()+3600);
+				//header("Location: index.php");
+			}		
+			else
+			{	
+				echo $login;
+				setcookie("user",$id, time()+3600);
+				//After signin user sees new index.html page as he is logged in, so sees logout and Logged In: UserName
+				header("Location: index1.html");
+			}
+			
+		}	
+		else
+		{ 
+			//If did not submit, return to original index.html without signin
+			header("Location: index.html");     
+			exit;
+		}
+	}
+}	
 oci_free_statement($statement);
 oci_close($connection);
--- restaurant/php/signup.php
+++ restaurant/php/signup.php
@@ -1,7 +1,78 @@
 <?php
+session_start();
+$valid=false;
+$connection = oci_connect($username = 'jing',
+                          $password = 'spring123456',
+                          $connection_string = '//oracle.cise.ufl.edu/orcl');
 
-/*
-search by city, name, zipcode here
-*/
+if (!$connection) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
 
+if (!isset ($_SESSION['user']))
+{
+	//echo "abc";
+	if (isset ($_POST['submit']))
+	{	$valid =true;
+		
+		$user = $_POST['email'];
+        $pass = $_POST['password'];
+        $fname = $_POST['fname'];
+		$lname = $_POST['lname'];
+		$city = $_POST['city'];
+		$state = $_POST['state'];
+		$street = $_POST['street'];
+		$zip = $_POST['zipcode'];
+		$date = $_POST['birthdate'];
+		$gen = $_POST['gender'];
+		//put default unknown user pic path in image url initially
+		$iurl = 'images/pic.jpg';
+		$level = 'Gourmet';
+		date_default_timezone_set('America/New_York');
+		$usersince = date('Y/m/d H:i:s');
+		$_SESSION['user']=$user;
+		if(empty($user) || empty($fname) || empty($lname) ||empty($pass) || empty($city) || empty($state)){ $valid = false; }
+		if($valid == true)
+		{	
+			$sql = "SELECT EMAIL FROM useraccount where EMAIL = '{$user}'";
+			$query = oci_parse($connection,$sql);
+			oci_execute($query);
+			if(oci_num_rows($query) > 0)
+			{
+				echo "The name is already registered";
+			}
+			else
+			{	
+				//take userid as auto increment column so use default
+				$sql = "INSERT into useraccount values(DEFAULT,'$pass','$fname','$lname','$iurl','$street','$zip','$user','$gen','$date','$level','$usersince','$city','$state')";
+				$query = oci_parse($connection,$sql);
+				oci_execute($query);
+							
+				if(!$query)
+				{
+				  echo "Failed ".oci_error();
+				}
+				else
+				{
+				  echo "Successful";
+				  // setcookie("user",$name, time()+3600);
+				  session_start();
+				  setcookie("user",$name, time()+3600);
+				  //After signup user sees new index.html page as he is logged in, so sees logout and Logged In: UserName
+				  header("Location: index1.php");
+				}
+			}		
+		}
+	
+		else
+		{ 
+			echo "Enter required fields(Email-ID, Password, First name, Last name, City, State)";			 
+		}
+	}
+}
+else
+{ 
+echo "<meta http-equiv='refresh' content='0;index1.php'>";
+}
 ?>
