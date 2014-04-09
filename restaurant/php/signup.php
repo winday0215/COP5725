6fe11851a9d9dd1e00755dd3f44e342ea31cf36b
winday0215 <winday0215@yahoo.com>
2014-04-08 22:48:22 -0400

    10:48pm

--- restaurant/php/signup.php
+++ restaurant/php/signup.php
@@ -1,5 +1,12 @@
 <?php
+
+/**************
+
+no session start here,
+session only start when sign in
+***************/
 session_start();
+
 $valid=false;
 $connection = oci_connect($username = 'jing',
                           $password = 'spring123456',
@@ -31,7 +38,13 @@ if (!isset ($_SESSION['user']))
 		$level = 'Gourmet';
 		date_default_timezone_set('America/New_York');
 		$usersince = date('Y/m/d H:i:s');
+		
+		/**************
+
+no any session info  when sign up
+***************/
 		$_SESSION['user']=$user;
+		
 		if(empty($user) || empty($fname) || empty($lname) ||empty($pass) || empty($city) || empty($state)){ $valid = false; }
 		if($valid == true)
 		{	
@@ -45,6 +58,11 @@ if (!isset ($_SESSION['user']))
 			else
 			{	
 				//take userid as auto increment column so use default
+				
+				/*************
+				query the largest number in userid field at first
+				then add 1 as the new userid
+				****************/
 				$sql = "INSERT into useraccount values(DEFAULT,'$pass','$fname','$lname','$iurl','$street','$zip','$user','$gen','$date','$level','$usersince','$city','$state')";
 				$query = oci_parse($connection,$sql);
 				oci_execute($query);
@@ -57,9 +75,21 @@ if (!isset ($_SESSION['user']))
 				{
 				  echo "Successful";
 				  // setcookie("user",$name, time()+3600);
+				  
+				  /***************
+				  no any session or info  when sign up
+				  *******************/
 				  session_start();
+				  
+				  /***************
+				  set cookie with userid, userid is unique
+				  *******************/
 				  setcookie("user",$name, time()+3600);
 				  //After signup user sees new index.html page as he is logged in, so sees logout and Logged In: UserName
+				  
+				  /***************
+				  popup an alert window, tell user he has successfully signed up, let him use this info to login, then go to signin.html
+				  *******************/
 				  header("Location: index1.php");
 				}
 			}		
@@ -73,6 +103,6 @@ if (!isset ($_SESSION['user']))
 }
 else
 { 
-echo "<meta http-equiv='refresh' content='0;index1.php'>";
+echo "<meta http-equiv='refresh' content='0;index.php'>";
 }
 ?>
